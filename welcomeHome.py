57683347b1075ee4d17c49be17694336cd1fc275
Craige McWhirter <craige@mcwhirter.io>
2016-11-06 14:14:52 +1000

    Initial commit of welcomeHome

---
+++ welcomeHome.py
@@ -0,0 +1,11 @@
+import mcpi.minecraft as minecraft
+import time
+
+mc = minecraft.Minecraft.create()
+
+while True:
+    time.sleep(1)
+    pos = mc.player.getTilePos()
+
+    if pos.x == 59 and pos.z == -11:
+        mc.postToChat("Welcome home!")
