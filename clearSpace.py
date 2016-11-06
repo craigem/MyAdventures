dfa26632cba4c48345f0b59aa9353bec562b0b5b
Craige McWhirter <craige@mcwhirter.io>
2016-11-06 23:19:22 +1000

    Centered clearing around player

--- clearSpace.py
+++ clearSpace.py
@@ -1,10 +1,21 @@
+# Clear some space near you.
+
+# Import necessary modules
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 
+# Connect to Minecraft
 mc = minecraft.Minecraft.create()
 
+# Get your position:
 pos = mc.player.getTilePos()
-size = int(raw_input("Size of area to clear? "))
 
+# Get the size of the space to clear
+size = int(raw_input("Size of area to clear? ")) / 2
+
+# Clear a space by setting it to AIR
 mc.setBlocks(
-    pos.x, pos.y, pos.z, pos.x + size, pos.y + size, pos.z + size, block.AIR.id)
+    pos.x - size, pos.y - 2, pos.z - size,
+    pos.x + size, pos.y + 100, pos.z + size,
+    block.AIR.id
+    )
