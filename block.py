b276646991771ad2ed10733d727c7f799f2e9ba1
Craige McWhirter <craige@mcwhirter.io>
2016-11-06 17:35:12 +1000

    Completed block.py

---
+++ block.py
@@ -0,0 +1,8 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc = minecraft.Minecraft.create()
+
+pos = mc.player.getTilePos()
+
+mc.setBlock(pos.x + 3, pos.y, pos.z, block.STONE.id)
