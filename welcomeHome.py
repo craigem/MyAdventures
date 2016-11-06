4b6c2025d28f951175a4a17b35c115b61550a0d3
Craige McWhirter <craige@mcwhirter.io>
2016-11-06 14:35:25 +1000

    Added y coordinate for greater accuracy

--- welcomeHome.py
+++ welcomeHome.py
@@ -7,5 +7,5 @@ while True:
     time.sleep(1)
     pos = mc.player.getTilePos()
 
-    if pos.x == 59 and pos.z == -11:
+    if pos.x == 59 and pos.y == -1 and pos.z == -11:
         mc.postToChat("Welcome home!")
