84df9cf421b5de8ae028f2959dcc24cb43ac81e2
Craige McWhirter <craige@mcwhirter.io>
2016-11-06 21:28:48 +1000

    Added keyboard input

--- clearSpace.py
+++ clearSpace.py
@@ -4,6 +4,7 @@ import mcpi.block as block
 mc = minecraft.Minecraft.create()
 
 pos = mc.player.getTilePos()
+size = int(raw_input("Size of area to clear? "))
 
 mc.setBlocks(
-    pos.x, pos.y, pos.z, pos.x + 50, pos.y + 50, pos.z + 50, block.AIR.id)
+    pos.x, pos.y, pos.z, pos.x + size, pos.y + size, pos.z + size, block.AIR.id)
