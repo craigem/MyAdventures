d25752e803f93d7562d2579d20fd82a8ceb46ccf
Craige McWhirter <craige@mcwhirter.io>
2016-11-07 16:37:50 +1000

    Added vanishing bridge feature

--- vanishingBridge.py
+++ vanishingBridge.py
@@ -8,6 +8,9 @@ import time
 # Connect to Minecraft
 mc = minecraft.Minecraft.create()
 
+# Initialise the bridge list
+bridge = []
+
 # The buildBridge function:
 def buildBridge():
     # get the player's position
@@ -17,6 +20,15 @@ def buildBridge():
     # Work out if the player is safe or not
     if b == block.AIR.id or b == block.WATER_STATIONARY.id or b == block.WATER_FLOWING.id:
         mc.setBlock(pos.x, pos.y - 1, pos.z, block.GLASS.id)
+        coordinate = [pos.x, pos.y - 1, pos.z]
+        bridge.append(coordinate)
+    elif b != block.GLASS.id:
+        if len(bridge) > 0:
+            coordinate = bridge.pop()
+            mc.setBlock(coordinate[0], coordinate[1],
+                coordinate[2], block.AIR.id)
+            time.sleep(0.25)
 
 while True:
+    time.sleep(0.25)
     buildBridge()
