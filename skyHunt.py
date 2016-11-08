d7e8aa626c2942e25fde598a0ef8435abf04581b
Craige McWhirter <craige@mcwhirter.io>
2016-11-08 20:51:10 +1000

    Wrote placeTresure.

--- skyHunt.py
+++ skyHunt.py
@@ -17,9 +17,18 @@ RANGE = 5
 
 # Initialise the coordinates of the treasure
 treasure_x = None
+treasure_y = None
+treasure_z = None
 
 def placeTreasure():
-    print("placeTreasure")
+    global treasure_x, treasure_y, treasure_z
+    # Get the player's position
+    pos = mc.player.getTilePos()
+    # randomly place the  treasure no more than RANGE away.
+    treasure_x = random.randint(pos.x, pos.x + RANGE)
+    treasure_y = random.randint(pos.y +2, pos.y + RANGE)
+    treasure_z = random.randint(pos.z, pos.z + RANGE)
+    mc.setBlock(treasure_x, treasure_y, treasure_z, block.DIAMOND_BLOCK.id)
 
 def checkHit():
     print("checkHit")
