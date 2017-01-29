5ef025c2a3c06877c157c8a37f07460ac595766c
Craige McWhirter <craige@mcwhirter.io>
2017-01-29 23:01:05 +1000

    Added missing and shortened lines

--- magicBridge.py
+++ magicBridge.py
@@ -8,6 +8,7 @@ import time
 # Connect to Minecraft
 mc = minecraft.Minecraft.create()
 
+
 # The buildBridge function:
 def buildBridge():
     # get the player's position
@@ -15,8 +16,11 @@ def buildBridge():
     # get the block under the player's feet
     b = mc.getBlock(pos.x, pos.y - 1, pos.z)
     # Work out if the player is safe or not
-    if b == block.AIR.id or b == block.WATER_STATIONARY.id or b == block.WATER_FLOWING.id:
+    if b == block.AIR.id
+    or b == block.WATER_STATIONARY.id
+    or b == block.WATER_FLOWING.id:
         mc.setBlock(pos.x, pos.y - 1, pos.z, block.GLASS.id)
+
 
 while True:
     buildBridge()
