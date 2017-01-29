5fe7fb30f5c12af5dc590ea77d1a3c83e440573d
Craige McWhirter <craige@mcwhirter.io>
2017-01-29 23:04:01 +1000

    Added missing and shortened lines

--- safeFeet.py
+++ safeFeet.py
@@ -8,6 +8,7 @@ import time
 # Connect to Minecraft
 mc = minecraft.Minecraft.create()
 
+
 # The safeFeet function:
 def safeFeet():
     # get the player's position
@@ -15,10 +16,13 @@ def safeFeet():
     # get the block under the player's feet
     b = mc.getBlock(pos.x, pos.y - 1, pos.z)
     # Work out if the player is safe or not
-    if b == block.AIR.id or b == block.WATER_STATIONARY.id or b == block.WATER_FLOWING.id:
+    if b == block.AIR.id
+    or b == block.WATER_STATIONARY.id
+    or b == block.WATER_FLOWING.id:
         mc.postToChat("not safe")
     else:
         mc.postToChat("safe")
+
 
 while True:
     time.sleep(0.5)
