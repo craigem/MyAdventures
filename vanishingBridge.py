035804bf1893a798ce373861d002a767ca63b39c
Craige McWhirter <craige@mcwhirter.io>
2017-01-29 23:15:34 +1000

    Added missing and shortened lines

--- vanishingBridge.py
+++ vanishingBridge.py
@@ -11,6 +11,7 @@ mc = minecraft.Minecraft.create()
 # Initialise the bridge list
 bridge = []
 
+
 # The buildBridge function:
 def buildBridge():
     # get the player's position
@@ -18,16 +19,21 @@ def buildBridge():
     # get the block under the player's feet
     b = mc.getBlock(pos.x, pos.y - 1, pos.z)
     # Work out if the player is safe or not
-    if b == block.AIR.id or b == block.WATER_STATIONARY.id or b == block.WATER_FLOWING.id:
+    if b == block.AIR.id
+    or b == block.WATER_STATIONARY.id
+    or b == block.WATER_FLOWING.id:
         mc.setBlock(pos.x, pos.y - 1, pos.z, block.GLASS.id)
         coordinate = [pos.x, pos.y - 1, pos.z]
         bridge.append(coordinate)
     elif b != block.GLASS.id:
         if len(bridge) > 0:
             coordinate = bridge.pop()
-            mc.setBlock(coordinate[0], coordinate[1],
-                coordinate[2], block.AIR.id)
+            mc.setBlock(
+                coordinate[0], coordinate[1],
+                coordinate[2], block.AIR.id
+                )
             time.sleep(0.25)
+
 
 while True:
     time.sleep(0.25)
